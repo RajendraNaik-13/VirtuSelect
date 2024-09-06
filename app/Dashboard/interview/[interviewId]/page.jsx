0f32b86a811a11e74d2f78747bf9fe56b3f92c0e
gokuler <133796167+gokuler94@users.noreply.github.com>
2024-09-06 19:50:07 +0530

    f

--- app/Dashboard/interview/[interviewId]/page.jsx
+++ app/Dashboard/interview/[interviewId]/page.jsx
@@ -7,8 +7,8 @@ import { useState } from 'react'
 import Webcam from 'react-webcam' 
 import { WebcamIcon } from 'lucide-react'      
 import { Button } from '@/components/ui/button'
-
-
+import { Lightbulb } from 'lucide-react'
+import  Link  from 'next/link'
 function interview({params}) {
     const [interviewData,setInterviewData]=useState();
     const [webCamEnabled,setWebCamEnabled]=useState(false);
@@ -25,8 +25,20 @@ function interview({params}) {
     };
     
   return (
-    <div className='my-10 flex justify-center flex-col items-center'>
+    <div className='my-10'>
         <h2 className='font-bold text-2xl'>Let's get started</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+          <div className='flex flex-col my-5 gap-5 '>
+          <div className='flex flex-col  gap-5 p-5 rounded-lg border'>
+          <h2 className='text-lg'><strong>job position:</strong>{interviewData ? interviewData.jobPosition : "Loading"}</h2>
+          <h2 className='text-lg'><strong>job discription:</strong>{interviewData ? interviewData.jobDesc : "Loading"}</h2>
+          <h2 className='text-lg'><strong>job Experience:</strong>{interviewData ? interviewData.jobExperience : "Loading"}</h2>
+        </div>
+        <div className='p-5 border-yellow-200 bg-yellow-200 rounded-lg'>
+        <h2 className='flex gap-2 items-center'><Lightbulb /><strong>Information</strong></h2>
+        <h2>{process.env.NEXT_PUBLIC_INFORMATION}</h2>
+        </div>
+        </div>
         <div>
             {webCamEnabled? <Webcam
             onUserMedia={()=>setWebCamEnabled(true)}
@@ -41,12 +53,17 @@ function interview({params}) {
             :
             <>
             <WebcamIcon className='h-72 w-full my-7 p-20 bg-secondary rounded-lg border'/>
-            <Button onClick={()=>setWebCamEnabled(true)}>Enable Cam and Microphone</Button>
+            <Button variant="" className=' ' onClick={()=>setWebCamEnabled(true)}>Enable Cam and Microphone</Button>
             </>
             }
         </div>
+        </div>
+        
         <div className='flex flex-col my-5'>
          </div>
+         <div className='flex justify-end items-end'>
+       <Link href={'/Dashboard/interview/'+params.interviewId+'/start'}><Button>Start Interview</Button></Link>
+       </div>  
     </div>
   )
 }
