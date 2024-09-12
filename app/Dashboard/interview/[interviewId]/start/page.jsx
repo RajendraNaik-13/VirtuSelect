f00be2d96c9af3d0d9bd25283aeeb940cd371e87
gokuler <133796167+gokuler94@users.noreply.github.com>
2024-09-12 18:13:09 +0530

    gg

--- app/Dashboard/interview/[interviewId]/start/page.jsx
+++ app/Dashboard/interview/[interviewId]/start/page.jsx
@@ -10,6 +10,7 @@ function StartInterview({params}) {
   const [interviewData,setInterviewData]=useState();
   const [mockInterviewQuestion,setMockInterviewQuestion]=useState();
   const [activeQuestionIndex,setActiveQuestionIndex]=useState(0);
+  var mock ;
   useEffect(()=>{
     GetInterviewDetails();
   },[]);
@@ -17,10 +18,13 @@ function StartInterview({params}) {
     const result=await db.select().from(MockInterview)
     .where(eq(MockInterview.mockId, params.interviewId))
     const jsonMockResp=JSON.parse(result[0].jsonMockResp);
-    console.log(jsonMockResp)
+    console.log("no:",jsonMockResp)
     setMockInterviewQuestion(jsonMockResp);
     setInterviewData(result[0]);
+    mock=result[0].mockId;
+    
 };
+console.log(interviewData?.mockId);
   return (
     <div>
       <div className='grid grid-col-1 md:grid-cols-2 gap-10'>
