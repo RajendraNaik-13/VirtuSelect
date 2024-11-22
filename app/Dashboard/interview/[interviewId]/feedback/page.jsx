a9db52425c5325bfd639f53379a8201f69ba738f
Rajendra Naik <rajendranaik1311@gmail.com>
2024-11-22 23:56:55 +0530

    landing page

---
+++ app/Dashboard/interview/[interviewId]/feedback/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function 
+h2() {
+  return (
+    <div className='p-10'>
+        <h2 className='text-3xl font-bold text bg-green-500'>Congradulations!</h2>
+        <h2 className='font-bold text-2xl'>Here is your interview feedback</h2>
+    </div>
+  )
+}
+
+export default 
+h2
